3c1db805ec59a331d9d03f15fc129ae0d27bb404
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 13:12:34 -0300

    actu5

--- src/app.js
+++ src/app.js
@@ -9,11 +9,7 @@ const app = express()
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
