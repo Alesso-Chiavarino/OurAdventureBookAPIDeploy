e36ebadef69d4d5c5cda88aa394f71624d30987c
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 15:42:14 -0300

    actu15 final

--- src/app.js
+++ src/app.js
@@ -9,36 +9,9 @@ const app = express()
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// app.use(cors());
-
-const allowCors = (fn) => async (req, res) => {
-
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // another option
-    // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-  
-    return await fn(req, res)
-  }
-  
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  }
-
-app.use((res, req, next) => {
-    allowCors(handler)
-    next()
-})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './upload',
