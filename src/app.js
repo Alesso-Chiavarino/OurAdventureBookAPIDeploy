39d81cca8424958f23fa5b59f38677826c66f2b3
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 04:26:47 -0300

    vercel4

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import express from 'express'
 import apiRoutes from './routes/app.routes.js'
 import fileUpload from 'express-fileupload'
 import cors from 'cors'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+// import { dirname, resolve } from 'path'
+// import { fileURLToPath } from 'url'
 
 const app = express()
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +17,11 @@ app.use(fileUpload({
 }))
 app.use(cors());
 app.use('/api', apiRoutes);
-app.use(express.static(resolve(__dirname, '../../client/build')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// app.use(express.static(resolve(__dirname, '../../client/build')));
 
 export default app;
