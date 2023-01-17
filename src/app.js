e08cecefffd83e345d4ffa722c82d03d18704de3
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 14:12:50 -0300

    actu13

--- src/app.js
+++ src/app.js
@@ -10,16 +10,31 @@ const app = express()
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
+
+const allowCors = (fn) => async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another option
+    // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+  
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+
 app.use((res, req, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    allowCors(handler)
     next()
 })
 app.use(express.json());
