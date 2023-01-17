9e2af723f6c53aaf72b88a84325f75227987bfcd
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 12:45:33 -0300

    actu

--- src/controllers/letters.controller.js
+++ src/controllers/letters.controller.js
@@ -2,21 +2,28 @@ import Letter from "../models/Letter.js"
 
 export const getLetters = async (req, res) => {
 
-    const { page } = req.query;
+    try {
+        const { page } = req.query;
 
-    const letters = await Letter.paginate({},{
-        page,
-        limit: 9
-    })
+        const letters = await Letter.paginate({},{
+            page,
+            limit: 9
+        })
 
-    if(!letters) {
-        return res.json({
+        if(!letters) {
+            return res.json({
+                status: "error",
+                error: "No letters found"
+            })
+        }
+
+        res.json(letters);
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "No letters found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(letters);
 
 }
 
@@ -24,32 +31,48 @@ export const getLetter = async (req, res) => {
 
     const { id } = req.params;
 
-    const letter = await Letter.findById(id)
+    try {
+        const letter = await Letter.findById(id)
 
-    if(!letter) {
-        return res.json({
+        if(!letter) {
+            return res.json({
+                status: "error",
+                error: "No letter found"
+            })
+        }
+
+        res.json(letter);
+
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "No letter found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(letter);
 }
 
 export const createLetter = async (req, res) => {
 
     const {title, description, author, date} = req.body;
 
-    const letter = await Letter.create({
-        title,
-        description,
-        author,
-        date
-    })
-
-    await letter.save();
-
-    res.json(letter);
+    try {
+        const letter = await Letter.create({
+            title,
+            description,
+            author,
+            date
+        })
+    
+        await letter.save();
+    
+        res.json(letter);
+        
+    } catch {
+        res.status(500).json({
+            status: "error",
+            error: "Something went wrong"
+        })
+    }
 }
 
 export const updateLetter = async (req, res) => {
@@ -57,21 +80,29 @@ export const updateLetter = async (req, res) => {
     const {title, description, author, date} = req.body;
     const {id} = req.params;
 
-    const updatedLetter = await Letter.findByIdAndUpdate(id, {
-        title,
-        description,
-        author,
-        date
-    })
+    try {
+        const updatedLetter = await Letter.findByIdAndUpdate(id, {
+            title,
+            description,
+            author,
+            date
+        })
+    
+        if(!updatedLetter) {
+            return res.json({
+                status: "error",
+                error: "No letter found"
+            })
+        }
+    
+        res.json(updatedLetter);
 
-    if(!updatedLetter) {
-        return res.json({
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "No letter found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(updatedLetter);
 
 }
 
@@ -79,14 +110,22 @@ export const deleteLetter = async (req, res) => {
 
     const { id } = req.params;
 
-    const removedLetter = await Letter.findByIdAndDelete(id)
+    try {
+        const removedLetter = await Letter.findByIdAndDelete(id)
 
-    if(!removedLetter) {
-        return res.json({
+        if(!removedLetter) {
+            return res.json({
+                status: "error",
+                error: "No letter found"
+            })
+        }
+
+        res.json(removedLetter);
+
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "No letter found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(removedLetter);
 }
--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -3,34 +3,41 @@ import Task from "../models/Task.js";
 export const getTasks = async (req, res) => {
 
     const {page, isDone} = req.query;
-    // const {page} = req.query;
 
-    if(req.query.isDone) {
-        const tasks = await Task.paginate({
-            done: isDone
-        }, {
+    try {
+        if(req.query.isDone) {
+            const tasks = await Task.paginate({
+                done: isDone
+            }, {
+                limit: 12,
+                page
+            })
+    
+            return res.json(tasks)
+        }
+    
+        if(req.query.page) {
+            const tasks = await Task.paginate({
+            }, {
+                limit: 12,
+                page
+            })
+    
+            return res.json(tasks)
+        }
+    
+        const tasks = await Task.paginate({}, {
             limit: 12,
-            page
+        });
+    
+        res.json(tasks);
+
+    } catch {
+        res.status(500).json({
+            status: "error",
+            error: "Something went wrong"
         })
-
-        return res.json(tasks)
     }
-
-    if(req.query.page) {
-        const tasks = await Task.paginate({
-        }, {
-            limit: 12,
-            page
-        })
-
-        return res.json(tasks)
-    }
-
-    const tasks = await Task.paginate({}, {
-        limit: 12,
-    });
-
-    res.json(tasks);
 
 }
 
@@ -38,16 +45,24 @@ export const getTask = async (req, res) => {
 
     const {id} = req.params;
 
-    const task = await Task.findById(id);
+    try {
+        const task = await Task.findById(id);
 
-    if(!task) {
-        return res.status(404).json({
+        if(!task) {
+            return res.status(404).json({
+                status: "error",
+                error: "Task not found"
+            })
+        }
+
+        res.json(task);
+
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "Task not found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(task);
     
 }
 
@@ -55,20 +70,29 @@ export const createTask = async (req, res) => {
 
     const {title, description, date, done} = req.body;
 
-    const task = new Task({
-        title,
-        description,
-        date,
-        done
-    })
+    try {
 
-    await task.save();
+        const task = new Task({
+            title,
+            description,
+            date,
+            done
+        })
+    
+        await task.save();
+    
+        res.json({
+            status: "success",
+            message: "Task created successfully",
+            task
+        });
 
-    res.json({
-        status: "success",
-        message: "Task created successfully",
-        task
-    });
+    } catch {
+        res.status(500).json({
+            status: "error",
+            error: "Something went wrong"
+        })
+    }
 }
 
 export const updateTask = async (req, res) => {
@@ -77,38 +101,56 @@ export const updateTask = async (req, res) => {
 
     const {title, description, date, done} = req.body;
 
-    const updatedTask = await Task.findByIdAndUpdate(id, {
-        title: title,
-        description: description,
-        date: date,
-        done: done
-    })
+    try {
 
-    if(!updatedTask) {
-        return res.status(404).json({
+        const updatedTask = await Task.findByIdAndUpdate(id, {
+            title: title,
+            description: description,
+            date: date,
+            done: done
+        })
+    
+        if(!updatedTask) {
+            return res.status(404).json({
+                status: "error",
+                error: "Task not found"
+            })
+        }
+    
+        res.json(updatedTask);
+
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "Task not found"
+            error: "Something went wrong"
         })
     }
-
-    res.json(updatedTask);
 }
 
 export const deleteTask = async (req, res) => {
     const { id } = req.params;
 
-    const deletedTask = await Task.findByIdAndDelete(id);
+    try {
 
-    if(!deletedTask) {
-        return res.status(404).json({
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if(!deletedTask) {
+            return res.status(404).json({
+                status: "error",
+                error: "Task not found"
+            })
+        }
+
+        res.json({
+            status: "success",
+            message: "Task deleted successfully",
+            deletedTask
+        });
+
+    } catch {
+        res.status(500).json({
             status: "error",
-            error: "Task not found"
+            error: "Something went wrong"
         })
     }
-
-    res.json({
-        status: "success",
-        message: "Task deleted successfully",
-        deletedTask
-    });
 }
